1ad982d197a05a39c717acf9688e27e1c8c237e2
Arbee <reniewritescode@gmail.com>
2024-09-24 16:49:14 +0800

    Added popup component

---
+++ src/components/Popup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Popup = () => {
+  return (
+    <div className='w-[688px] h-[197px] bg-white rounded-lg overflow-hidden '>
+      <div className='w-full h-[74px] bg-Green_base font-medium flex items-center px-8'>
+        <h1 className='text-white text-2xl'>GENERATE REPORT</h1>
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi dolorum eius vel odit porro. Et, ad eligendi voluptatem optio cumque accusamus nobis suscipit dignissimos magnam, nihil distinctio atque ab! Sunt!</p>
+    </div>
+  )
+}
+
+export default Popup
