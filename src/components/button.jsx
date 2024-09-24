17774c2ac870049aceb8dc47407f046befac9cd3
Wraithzero202 <joshua.libando458@gmail.com>
2024-09-24 17:35:44 +0800

    ButtonsCreation

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,15 @@
-const Button = () => {
-    return (<div>
-        <img src="" alt="" />
-    </div>);
-};
+export function ConfirmButton(){
+    return (
+        <button className="w-[114px] h-[30px] bg-Green_Button text-white rounded-[3px]">
+            CONFIRM
+        </button>
+    );
+}
 
-export default Button;
+export function CancelButton(){
+    return (
+        <button className="w-[114px] h-[30px] bg-white text-black rounded-[3px] border-[0.5px] border-black">
+            CANCEL
+        </button>
+    );
+}
